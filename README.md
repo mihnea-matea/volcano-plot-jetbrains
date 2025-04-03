f1848bd8ed89ee17bb4e8525857b4e72dfd2baaf
Mihnea Matea <m.matea@student.tudelft.nl>
2025-04-03 05:03:10 +0200

    Add data about other papers using MyGene

---
+++ README.md
@@ -0,0 +1,10 @@
+- Volcano plot: Flask-based web app to visualize gene data in a plot built using plotly.js
+- Clickable points: opens boxplots for each gene that is clicked
+- Uses data from the Excel file:  
+  `NIHMS1635539-supplement-1635539_Sup_tab_4.xlsx`
+
+# Installation 
+- Clone repository (using git clone) and install dependencies (using pip install flask pandas numpy openpyxl requests)
+- Type the following command in order to run the app locally: python app.py
+- Open browser and go to : http://127.0.0.1:5000
+
--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, jsonify
 import pandas
 import numpy as np
+import requests
 
 app = Flask(__name__)
 
@@ -45,6 +46,43 @@ def get_boxplot_data(gene_name):
         "young": young_values,
         "old": old_values
     })
+
+@app.route("/api/gene-info/<gene_name>")
+def gene_info(gene_name):
+    try:
+        query = f"symbol:{gene_name}&species=human"
+        url = f"https://mygene.info/v3/query?q={query}"
+
+        response = requests.get(url)
+        data = response.json()
+        if "hits" not in data or len(data["hits"]) == 0:
+            return jsonify({
+                "papers": [],
+                "error": f"No gene found for symbol: {gene_name}"
+            })
+        gene_id = data["hits"][0]["_id"]
+        detail_url = f"https://mygene.info/v3/gene/{gene_id}"
+        details = requests.get(detail_url).json()
+        papers = []
+        if "generif" in details:
+            for entry in details["generif"]:
+                if "pubmed" in entry and "text" in entry:
+                    paper_info = {
+                        "pmid": entry["pubmed"],
+                        "title": entry["text"],
+                        "url": f"https://pubmed.ncbi.nlm.nih.gov/{entry['pubmed']}/"
+                    }
+                    papers.append(paper_info)
+        return jsonify({
+            "gene": gene_name,
+            "papers": papers[:5] # we will only show the first 5 such associations (could be too many) 
+        })
+
+    except Exception as err:
+        return jsonify({
+            "error": str(err)
+        })
+
 
 if __name__ == "__main__":
     app.run(debug=True)
