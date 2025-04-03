fa7ad2c7f2613c1d75817484c336954bb75bbc78
Mihnea Matea <m.matea@student.tudelft.nl>
2025-04-03 03:51:55 +0200

    Read data from excel file

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 from flask import Flask
+import pandas
 
 app = Flask(__name__)
+
+excel_file = "data/NIHMS1635539-supplement-1635539_Sup_tab_4.xlsx"
+
+S4B_df = pandas.read_excel(excel_file, sheet_name="S4B limma results", skiprows = 2)
+S4A_df = pandas.read_excel(excel_file, sheet_name="S4A values", skiprows = 2)
 
 @app.route("/")
 def index():
